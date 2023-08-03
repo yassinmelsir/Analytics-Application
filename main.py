08b57d8f4f33a858942bca4fa8e6b7c8da4cd40d
Yassin Magdi Elsir <yme@MBP-von-Yassin.fritz.box>
2023-08-03 18:29:50 +0200

    starting on data cleaning logic

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import tkinter as tk
+from tkinter import filedialog, messagebox
 import seaborn as sns
 import matplotlib.pyplot as plt
+from pymongo import MongoClient
 
 from matplotlib.backends.backend_tkagg import (
 FigureCanvasTkAgg)
@@ -11,21 +13,54 @@ serverAddress = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectio
 databaseName = 'blank' # give new database name
 collectionName  = 'blank' # give new collection name
 
+statscolumn, groupscolumn, visualizations, statistics = 'Births', 'Sex', ['Information','Correlation'], ['mean', 'std', 'min']
+
 filePath = '/Users/yme/Code/AdvancedProgramming/SummativeAssessment/top_3_names_per_sex.csv'
-data, workingdata, statscolumn, groupscolumn, visualizations, statistics = pd.read_csv(filePath), pd.read_csv(filePath), 'Births', 'Sex', ['Information','Correlation'], ['mean', 'std', 'min']
+data, workingdata = pd.read_csv(filePath), pd.read_csv(filePath)
 
 def on_exit():
     root.destroy()
     
 def on_loadfromcsv():
-    global data
-    data = pd.read_csv(filePath)
-    for child in root.winfo_children(): child.destroy()
+    global data, workingdata, filePaths
+    
+    filePaths = filedialog.askopenfilenames(title='Select The CSVs')
+    
+    cleandata()
+    save_to_mongo()
     workingscreen()
     
-def on_loadfromdatabase():
+def save_to_mongo():
     global data
-    data = pd.read_csv(filePath)
+    mongodata = data.to_dict(orient='records')
+    client, collection = connect_to_server()
+    collection.insert_many(mongodata)
+    client.close()
+
+def cleandata():
+    # global data, workingdata, filePaths
+    # dataframes = [pd.read_csv(filePath, encoding='latin1') for filePath in filePaths]
+    # for dataframe in dataframes:
+    #     if 'NGR' in dataframe.columns: antennas = dataframe
+    #     if 'EID' in dataframe.columns: params = dataframe
+    antenna, params = pd.read_csv('/Users/yme/Code/AdvancedProgramming/SummativeAssessment/TxAntennaDAB.csv', encoding='latin1'), pd.read_csv('/Users/yme/Code/AdvancedProgramming/SummativeAssessment/TxParamsDAB.csv', encoding='latin1')
+    breakpoint()
+    
+    
+def connect_to_server():
+    client = MongoClient(serverAddress)
+    database = client[databaseName]
+    collection = database[collectionName]
+    return client, collection
+    
+def on_loadfromdatabase():
+    global data, workingdata
+    client, collection = connect_to_server()
+    results = collection.find()
+    formatted_results = list(results)
+    data, workingdata = pd.DataFrame(formatted_results), pd.DataFrame(formatted_results)
+    client.close()
+    
     for child in root.winfo_children(): child.destroy()
     workingscreen()
 
@@ -39,28 +74,38 @@ def initialscreen():
     exit = tk.Button(root, text='Exit', command=on_exit)
     exit.pack(expand=True, fill=tk.BOTH)
     
+def dialog_box():
+    messagebox.showerror("Error", "File not found")
+    
 def workingscreen():
+    for child in root.winfo_children(): child.destroy()
     buttons()
     numericaldata()
-    graph()
-    
-    
-def graph():
+    graph_information()
+        
+def graph_information():
     global ax, canvas
     fig = Figure(figsize=(10,8), dpi=100)
     ax = fig.add_subplot()
     sns.lineplot(x='Year', y='Births', hue='Name', data=workingdata, ax=ax)
     ax.set_xticklabels(ax.get_xticklabels(),rotation=45, fontsize=10)  
     plt.tight_layout()
-    plt.savefig('activity_two.png', dpi=100)
     canvas = FigureCanvasTkAgg(fig, master=root)
     canvas.draw()
     canvas.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
     
-def regraph():
+def regraph_information():
     global ax, canvas
     ax.clear()
     sns.lineplot(x='Year', y='Births', hue='Name', data=workingdata, ax=ax)
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=45, fontsize=10)
+    plt.tight_layout()
+    canvas.draw()
+    
+def regraph_correlation():
+    global ax, canvas
+    ax.clear()
+    sns.barplot(x='Name', y='Births', data=workingdata, ax=ax)
     ax.set_xticklabels(ax.get_xticklabels(), rotation=45, fontsize=10)
     plt.tight_layout()
     canvas.draw()
@@ -75,12 +120,9 @@ def populate_numerical_frame():
     description = workingdata[statscolumn].describe() 
     for statistic in statistics:
         statisticframe = tk.Frame(numericaldataframe, relief='groove', borderwidth=1)
-        statisticlabel = tk.Label(statisticframe, text=statistic.capitalize()+':')
-        statisticdata = tk.Label(statisticframe, text=description[statistic])
+        statisticlabel = tk.Label(statisticframe, text=statistic.capitalize() + ': ' + str(description[statistic]))
         
-        statisticframe.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
-        statisticlabel.pack(side=tk.LEFT, fill=tk.BOTH)
-        statisticdata.pack(side=tk.LEFT, fill=tk.BOTH)
+        statisticlabel.pack(side=tk.LEFT, fill=tk.BOTH), statisticframe.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
         
 def repopulate_statistics():
     for child in numericaldataframe.winfo_children(): child.destroy()
@@ -118,19 +160,19 @@ def buttons():
     exit.pack(side=tk.RIGHT, fill=tk.BOTH)
     
 def update_visualization():
-    print('Visualization Updated')
+    regraph_information() if selected_visualization.get() == 'Information' else regraph_correlation()
 
 def update_data_range():
-    global workingdata
+    global workingdata, selected_visualization
     selected_groups = [group for group, selected in checkbox_variables.items() if selected.get()]
     workingdata = data[data[groupscolumn].isin(selected_groups)]
     repopulate_statistics()
-    regraph()
+    regraph_information() if selected_visualization.get() == 'Information' else regraph_correlation()    
     
     
 root = tk.Tk()
 
-workingscreen()
+cleandata()
 
 root.mainloop()
 
